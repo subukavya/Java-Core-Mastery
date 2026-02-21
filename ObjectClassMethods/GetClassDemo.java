d752793c6cb5ddcd0e9140708ba21ef45332c95c
Kavya N <kavyasubu1207@gmail.com>
2026-02-21 09:23:24 +0530

    Add GetClassDemo to showcase getClass() usage
    Demonstrates the use of getClass() method in Java.

---
+++ ObjectClassMethods/GetClassDemo.java
@@ -0,0 +1,13 @@
+/*
+ * getClass()
+ * ----------
+ * Returns runtime class of an object.
+ * Used for reflection and type checking.
+ */
+
+class GetClassDemo {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s.getClass());  // class java.lang.String
+    }
+}
